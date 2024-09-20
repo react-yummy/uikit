51f0b36c502be734c131db3029e9d2ba930cc1c8
unknown <risunyaaa@yandex.ru>
2024-09-21 00:01:39 +0300

    :bento: add vitest for unit-tests and react testing library for react-tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import dts from 'vite-plugin-dts'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(),  dts({
     tsconfigPath: resolve(__dirname, "tsconfig.app.json"),
@@ -29,5 +29,12 @@ export default defineConfig({
         }
       } 
     }
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './lib/test/setup.ts',
+    //let it be for now
+    css: true,
   }
 })
